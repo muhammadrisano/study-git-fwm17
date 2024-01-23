ba2e5f050464b20d0d2583f7849421b4a57cf899
muhammadrisano <muhammadrisano@gmail.com>
2024-01-23 10:40:41 +0700

    add desciptionRegister in file index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const name = "risano"
 const email = "risano@gmail.com"
 const fullName = "risano akbar"
+
+const descriptionRegister = "ketika register user menimputkan first name, last name, email, password "
