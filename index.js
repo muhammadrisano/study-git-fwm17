4d3e9537a0cdd2385a72b5f0915fbd6e7b51782f
muhammadrisano <muhammadrisano@gmail.com>
2024-01-23 10:52:47 +0700

    add alamat in file index.js

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ const email = "risano@gmail.com"
 const fullName = "risano akbar"
 const descriptionLogin = "user login menggunakan email dan passwrod"
 const descriptionRegister = "ketika register user menimputkan first name, last name, email, password "
+
+const alamat = "jalan jati gaung, kel, jati, kec padang timur, padang, sumater barat, indonesia"
